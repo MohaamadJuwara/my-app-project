f9d4930e09e7b9dcb9479f00cf2b7a7168adeee3
mohaamad juwara <mohaamadd.juwara@anove.ai>
2025-10-15 16:43:06 +0200

    Fix empty dashboard layout and add missing UI components

---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,19 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { lusitana } from './fonts';
+import Image from 'next/image';
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <Image
+        src="/juwaralogo.png"
+        alt="Juwara Solutions Logo"
+        width={100}
+        height={100}
+        className="rounded-full"
+      />
+    </div>
+  );
+}
